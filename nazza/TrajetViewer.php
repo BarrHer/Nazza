04b9f33e65a8de25bf4acdaede2191f0488ad339
BarrHe <herve974.30@gmail.com>
2019-04-27 10:39:46 +0400

    Fusion des 3 parties (mail+map+modal)

--- nazza/TrajetViewer.php
+++ nazza/TrajetViewer.php
@@ -1,5 +1,9 @@
 <?php include ("header.php");?>
 
+<script>
+var tab = Array();
+</script>
+
 <div class="container mt-5 py-4 rounded shadow-lg">
     <h3 class="text-center mb-5">Ajouter un nouveau trajet</h3>
     <form action="?ctrl=trajet&mth=add" method="post">
@@ -8,8 +12,17 @@
             <select id="inputAddress" name="inputAddress" class="form-control" onchange="verif(1);">
             <option value="0">--</option>
             <?php foreach ($villes as $key => $value) {
-                echo '<option value="'.$value['id_ville'].'" onClick="addMarkerA('.$value['longitude'].','.$value['latitude'].')" >'.$value['nom_ville'].'</option>';
-                } ?> 
+                echo '<option value="'.$value['id_ville'].'">'.$value['nom_ville'].'</option>';?>
+                <script>
+                
+                tab[<?php echo $value['id_ville']?>] = Array();
+                tab[<?php echo $value['id_ville']?>][1] = <?php echo $value['longitude']?>;
+                tab[<?php echo $value['id_ville']?>][2] = <?php echo $value['latitude']?>;
+                //console.log(tab[25]);
+                </script>
+
+                <?php } ?> 
+
             </select> 
         </div>
         <div class="form-group">
@@ -40,128 +53,20 @@
     </form>
 </div>
 
-<script>
-var ladate=new Date();
-var date = ladate.toISOString().split('T')[0];
-var heure = ladate.toTimeString().split(' ')[0];
-//document.getElementById('trajetHeure').value = heure;
-//$("#trajetDate").val(date);
-$("#trajetHeure").val(heure);
-$(document).ready(function() {
-    $("#trajetDate").attr({
-       "min" : date,
-       "value" : date
-    });
-});
 
-function verif(input){
-    var ville1 = document.getElementById('inputAddress').value;
-    var ville2 = document.getElementById('inputAddress2').value;
-    if (input == 1){
-        if (ville1 != 1){
-        document.getElementById('inputAddress2').value = 1;
-        }
-        else {
-            document.getElementById('inputAddress2').value = 0;
-        }
-    }
-    else {
-        if (ville2 != 1){
-        document.getElementById('inputAddress').value = 1;
-        }
-        else {
-            document.getElementById('inputAddress').value = 0;
-        }
-    }
-    
-}
+<!-- function pour select option -->
+<script src="js/verif.js"></script>
 
-</script>
 
 <!-- Map -->
 
-
 <link rel="stylesheet" href="https://cdn.rawgit.com/openlayers/openlayers.github.io/master/en/v5.3.0/css/ol.css" type="text/css">
-    <style>
-      .map {
-        height: 500px;
-        width: 50%;
-        padding-left:0px;
-        padding-right:0px;
-      }
-    </style>
-
+<link rel="stylesheet" type="text/css" href="css/mapTraj.css">
 <script src="https://cdn.rawgit.com/openlayers/openlayers.github.io/master/en/v5.3.0/build/ol.js"></script>
 
 <div id="map" class="map container mt-3 "></div>
-    <script type="text/javascript">
-    $Long = 55.536384;
-    $Lat = -21.115141;
-    
-      var map = new ol.Map({
-        target: 'map',
-        layers: [
-          new ol.layer.Tile({
-            source: new ol.source.OSM()
-          })
-        ],
-        view: new ol.View({
-          center: ol.proj.fromLonLat([$Long,$Lat]),
-          zoom: 10
-        })
-      });
-    
 
-    function addMarkerA(LongA, LatA){
-        var markerA = new ol.Feature({
-            geometry: new ol.geom.Point(
-                ol.proj.fromLonLat([LongA,LatA])
-            ),
-        });
-        var vectorSourceA = new ol.source.Vector({
-            features: [markerA]
-        });
-        var markerVectorLayerA = new ol.layer.Vector({
-            source: vectorSourceA,
-        });
-        map.addLayer(markerVectorLayerA);
+<script type="text/javascript"  src="js/map.js" ></script>
 
-        markerA.setStyle(new ol.style.Style({
-            image: new ol.style.Icon(({
-                crossOrigin: 'anonymous',
-                src: 'image/a.png',
-                scale: 0.40
-            }))
-        }));
-    }
 
-    function addMarkerB(LongB, LatB){
-
-        var markerB = new ol.Feature({
-            geometry: new ol.geom.Point(
-                ol.proj.fromLonLat([LongB, LatB])
-            ),
-        });
-
-        var vectorSourceB = new ol.source.Vector({
-            features: [markerB],
-        });
-        var markerVectorLayerB = new ol.layer.Vector({
-            source: vectorSourceB,
-        });
-        
-        map.addLayer(markerVectorLayerB);
-
-        markerB.setStyle(new ol.style.Style({
-            image: new ol.style.Icon(({
-                crossOrigin: 'anonymous',
-                src: 'image/b.png',
-                
-                scale: 0.40
-            }))
-        }));
-    }
-
-</script> 
-<br><br><br><br>
 <?php include ("footer.php"); ?>
--- nazza/header.php
+++ nazza/header.php
@@ -6,10 +6,14 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="css/style.css">
   <link href="https://fonts.googleapis.com/css?family=ZCOOL+QingKe+HuangYou" rel="stylesheet">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://cdn.rawgit.com/openlayers/openlayers.github.io/master/en/v5.3.0/css/ol.css" type="text/css">
+  <link rel="stylesheet" type="text/css" href="css/mapIndex.css">
+  <script src="https://cdn.rawgit.com/openlayers/openlayers.github.io/master/en/v5.3.0/build/ol.js"></script>
+  <script type="text/javascript"  src="js/map.js" ></script>
 </head>
 <body class="bg-light" style="font-family: 'ZCOOL QingKe HuangYou', cursive; font-size: 18px;">
 
